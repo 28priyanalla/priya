8ba5c651f5318953d42c7dd7fb28194a5a7cc00d
DELL <DELL@DESKTOP-N43SQ8R>
2024-02-15 17:46:59 +0530

    Secondcommit

---
+++ src/day2/Singlelevelinheritence.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class Singlelevelinheritence {
+	public static void main(String[] args) {
+		Student s=new Student("priya","bangalore",8765479l,9845094342l);
+		System.out.println(s);
+		
+		Student s1=new Student(1,"sreyas");
+		System.out.println(s1);
+	}
+
+}
---
+++ src/day2/SuperDemo.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class SuperDemo {
+	int a=60;   // data member
+	 
+	 void drink() {
+		 System.out.println("tea");
+	 }
+	 
+	 SuperDemo(){  //constructor
+		 System.out.println("welcome");
+	 }
+}
